bcdd762dae3a201189bb0f4b5adf81df7c301e59
Geonseob Kim <109283556+gnksbm@users.noreply.github.com>
2025-06-28 18:58:40 +0900

    add EventMonitor

--- Sources/HTTP/HTTPProvider.swift
+++ Sources/HTTP/HTTPProvider.swift
@@ -8,16 +8,28 @@
 import Foundation
 
 public protocol HTTPProvider {
+    var eventMonitor: EventMonitor? { get }
+    
     func request<R: HTTPRequest>(_ request: R) async throws -> HTTPResponse
 }
 
 extension URLSession: HTTPProvider {
+    public var eventMonitor: EventMonitor? { nil }
+    
     public func request<R: HTTPRequest>(_ request: R) async throws -> HTTPResponse {
-        let urlRequest = try request.urlRequest
-        let (data, response) = try await data(for: urlRequest)
-        guard let httpURLResponse = response as? HTTPURLResponse else {
-            throw URLError(.badServerResponse)
+        eventMonitor?.didRequest(request)
+        do {
+            let urlRequest = try request.urlRequest
+            let (data, response) = try await data(for: urlRequest)
+            guard let httpURLResponse = response as? HTTPURLResponse else {
+                throw URLError(.badServerResponse)
+            }
+            let httpResponse = HTTPResponse(data: data, httpURLResponse: httpURLResponse)
+            eventMonitor?.didRecieve(request, httpURLResponse: httpResponse)
+            return httpResponse
+        } catch {
+            eventMonitor?.didError(request, error: error)
+            throw error
         }
-        return HTTPResponse(data: data, httpURLResponse: httpURLResponse)
     }
 }
